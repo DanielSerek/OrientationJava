23a82013854b402524060d2138369a5527dfe955
DanielSerek <daniel.serek.uk@gmail.com>
2020-07-06 16:22:17 +0200

    additional features in MyShop

---
+++ MyShop-final/src/main/java/com/myshop/myshop/models/ShopItem.java
@@ -0,0 +1,57 @@
+package com.myshop.myshop.models;
+
+public class ShopItem {
+    private String name;
+    private String type;
+    private String description;
+    private float price;
+    private int quantityOfStock;
+
+    public ShopItem(String name, String type, String description, float price, int quantityOfStock) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.price = price;
+        this.quantityOfStock = quantityOfStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantityOfStock() {
+        return quantityOfStock;
+    }
+
+    public void setQuantityOfStock(int quantityOfStock) {
+        this.quantityOfStock = quantityOfStock;
+    }
+}
